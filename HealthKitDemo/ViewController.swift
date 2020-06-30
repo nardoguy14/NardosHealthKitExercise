2f6631688bdb0ac0a12b2c4d1eb7a49841808214
Nardo Arevalo <barevalo@villagemd.com>
2020-06-29 23:49:14 -0700

    Initial Commit

---
+++ HealthKitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthKitDemo
+//
+//  Created by Bernardo Arevalo on 6/29/20.
+//  Copyright © 2020 Bernardo Arevalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
